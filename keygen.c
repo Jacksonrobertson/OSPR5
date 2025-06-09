5540fa33bf7e24bb2ca48a2590d502928aeeb907
Jackson Robertson <161077444+Jacksonrobertson@users.noreply.github.com>
2025-06-08 19:12:55 -0700

    Update keygen.c

--- keygen.c
+++ keygen.c
@@ -9,8 +9,10 @@ char gen_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 int main(int argc, char *argv[]) {
     long key = strtol(argv[1], NULL, 10);
     srand(time(NULL));
-    for (int i = 0; i < 256; i++) {
-        rand();
+    for (int i = 0; i < key; i++) {
+        int rand_num = rand() % 27;
+        putchar(gen_chars[rand_num]);
     }
-
+    putchar('\n');
+    return 0;
 }
