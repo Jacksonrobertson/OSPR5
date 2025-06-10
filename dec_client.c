1ea731c80ffc2d4045bd08bbc9a9a8f32ee52f45
Jackson Robertson <161077444+Jacksonrobertson@users.noreply.github.com>
2025-06-09 21:11:07 -0700

    Update dec_client.c

--- dec_client.c
+++ dec_client.c
@@ -16,7 +16,7 @@
 // Error function used for reporting issues
 void error(const char *msg) {
   perror(msg);
-  exit(0);
+  exit(1);
 }
 
 // Set up the address struct
@@ -36,7 +36,7 @@ void setupAddressStruct(struct sockaddr_in* address,
   struct hostent* hostInfo = gethostbyname(hostname);
   if (hostInfo == NULL) {
     fprintf(stderr, "CLIENT: ERROR, no such host\n");
-    exit(0);
+    exit(1);
   }
   // Copy the first IP address from the DNS entry to sin_addr.s_addr
   memcpy((char*) &address->sin_addr.s_addr,
@@ -48,11 +48,93 @@ int main(int argc, char *argv[]) {
   int socketFD, charsWritten, charsRead;
   struct sockaddr_in serverAddress;
   char buffer[256];
-  // Check usage & args
-  if (argc < 3) {
+  if (argc != 4) {
     fprintf(stderr,"USAGE: %s hostname port\n", argv[0]);
-    exit(0);
+    exit(1);
   }
+  //==if (argc < 4) {
+    //fprintf(stderr,"USAGE: %s key error\n", argv[0]);
+    //exit(1);
+  //}
+
+  FILE *input_file = fopen(argv[1], "r");
+  if (input_file == NULL) {
+    fprintf(stderr,"CLIENT: ERROR, Opening Input File\n");
+    exit(1);
+  }
+
+  FILE *key_file = fopen(argv[2], "r");
+  if (key_file == NULL) {
+    fprintf(stderr,"CLIENT: ERROR, Opening Key File\n");
+    fclose(input_file);
+    exit(1);
+  }
+
+  fseek(input_file, 0, SEEK_END);
+  int input_file_length = ftell(input_file);
+  rewind(input_file);
+
+  char input_buffer [input_file_length+1];
+
+  for (int i = 0; i < input_file_length; i++) {
+    int input_char = fgetc(input_file);
+    if (input_char == EOF) {
+      fprintf(stderr,"CLIENT: ERROR, KEY SIZE NOT CORRECT\n");
+      exit(1);
+    }
+    input_buffer[i] = input_char;
+  }
+  input_buffer[input_file_length] = '\0';
+
+  fseek(key_file, 0, SEEK_END);
+  int key_length = ftell(key_file);
+  rewind(key_file);
+
+  char key_buffer[key_length+1];
+
+  for (int i = 0; i < key_length; i++) {
+    int key_char = fgetc(key_file);
+    if (key_char == EOF) {
+      fprintf(stderr,"ERROR\n");
+      exit(1);
+    }
+    key_buffer[i] = key_char;
+  }
+  key_buffer[key_length] = '\0';
+
+  if (key_length < input_file_length) {
+    fprintf(stderr, "Error: key length\n");
+    exit(1);
+  }
+
+  if (input_buffer[input_file_length - 1] == '\n') {
+    input_file_length = input_file_length - 1;
+    input_buffer[input_file_length] = '\0';
+    }
+
+  if (key_buffer[key_length - 1] == '\n') {
+    key_length = key_length - 1;
+    key_buffer[key_length] = '\0';
+  }
+
+  for (int i = 0; i < input_file_length; i++) {
+    if ((input_buffer[i] < 'A' || input_buffer[i] > 'Z')) {
+      if (input_buffer[i] != ' ') {
+        fprintf(stderr, "Error: input file not A-Z\n");
+        exit(1);
+      }
+    }
+  }
+    for (int j = 0; j < key_length; j++) {
+      if ((key_buffer[j] < 'A' || key_buffer[j] > 'Z')) {
+        if (key_buffer[j] != ' ') {
+          fprintf(stderr, "Error: key not A-Z\n");
+          exit(1);
+        }
+      }
+    }
+  fclose(input_file);
+  fclose(key_file);
 
   // Create a socket
   socketFD = socket(AF_INET, SOCK_STREAM, 0);
@@ -61,42 +143,42 @@ int main(int argc, char *argv[]) {
   }
 
    // Set up the server address struct
-  setupAddressStruct(&serverAddress, atoi(argv[2]), argv[1]);
-
+  setupAddressStruct(&serverAddress, atoi(argv[3]), "localhost");
   // Connect to server
   if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
     error("CLIENT: ERROR connecting");
   }
-  // Get input message from user
-  printf("CLIENT: Enter text to send to the server, and then hit enter: ");
-  // Clear out the buffer array
-  memset(buffer, '\0', sizeof(buffer));
-  // Get input from the user, trunc to buffer - 1 chars, leaving \0
-  fgets(buffer, sizeof(buffer) - 1, stdin);
-  // Remove the trailing \n that fgets adds
-  buffer[strcspn(buffer, "\n")] = '\0';
 
-  // Send message to server
-  // Write to the server
-  charsWritten = send(socketFD, buffer, strlen(buffer), 0);
-  if (charsWritten < 0){
-    error("CLIENT: ERROR writing to socket");
-  }
-  if (charsWritten < strlen(buffer)){
-    printf("CLIENT: WARNING: Not all data written to socket!\n");
+  int i = 0;
+  while (i < input_file_length) {
+    int n = send(socketFD,input_buffer + i , input_file_length - i,0);
+    if (n == -1) {
+      error("Error: sending input file\n");
+    }
+    i += n;
   }
 
-  // Get return message from server
-  // Clear out the buffer again for reuse
-  memset(buffer, '\0', sizeof(buffer));
-  // Read data from the socket, leaving \0 at end
-  charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0);
-  if (charsRead < 0){
-    error("CLIENT: ERROR reading from socket");
+  int j = 0;
+  while (j < key_length) {
+    int n = send(socketFD, key_buffer + j, key_length - j, 0);
+    if (n == -1) {
+      error("Error: sending key\n");
+    }
+    j += n;
   }
-  printf("CLIENT: I received this from the server: \"%s\"\n", buffer);
 
-  // Close the socket
+  char *received_code = malloc(input_file_length + 1);
+    int received = 0;
+    while (received < input_file_length) {
+      int n = recv(socketFD,received_code + received, input_file_length - received, 0);
+      if (n <= 0) {
+        error("Error: receiving data\n");
+      }
+      received += n;
+    }
+  received_code[input_file_length] = '\0';
+  printf("%s\n", received_code);
+  free(received_code);
   close(socketFD);
   return 0;
 }
