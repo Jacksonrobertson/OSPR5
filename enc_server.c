bfd47d63a279f1e72bd8ebe82472c18d81492718
Jackson Robertson <161077444+Jacksonrobertson@users.noreply.github.com>
2025-06-09 23:25:53 -0700

    Update enc_server.c

--- enc_server.c
+++ enc_server.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <signal.h>
 
 // Error function used for reporting issues
 void error(const char *msg) {
@@ -28,8 +29,8 @@ void setupAddressStruct(struct sockaddr_in* address,
 }
 
 int main(int argc, char *argv[]){
-  int connectionSocket, charsRead;
-  char buffer[256];
+  signal(SIGCHLD, SIG_IGN);
+  int connectionSocket;
   struct sockaddr_in serverAddress, clientAddress;
   socklen_t sizeOfClientInfo = sizeof(clientAddress);
 
@@ -55,42 +56,88 @@ int main(int argc, char *argv[]){
     error("ERROR on binding");
   }
 
-  // Start listening for connetions. Allow up to 5 connections to queue up
+  // Start listening for connections. Allow up to 5 connections to queue up
   listen(listenSocket, 5);
 
   // Accept a connection, blocking if one is not available until one connects
   while(1){
     // Accept the connection request which creates a connection socket
-    connectionSocket = accept(listenSocket,
-                (struct sockaddr *)&clientAddress,
-                &sizeOfClientInfo);
+    connectionSocket = accept(listenSocket, (struct sockaddr *)&clientAddress, &sizeOfClientInfo);
+
     if (connectionSocket < 0){
       error("ERROR on accept");
+      continue;
     }
-
-    printf("SERVER: Connected to client running at host %d port %d\n",
-                          ntohs(clientAddress.sin_addr.s_addr),
-                          ntohs(clientAddress.sin_port));
-
-    // Get the message from the client and display it
-    memset(buffer, '\0', 256);
-    // Read the client's message from the socket
-    charsRead = recv(connectionSocket, buffer, 255, 0);
-    if (charsRead < 0){
-      error("ERROR reading from socket");
+    pid_t pid = fork();
+    if (pid < 0) {
+      perror("ERROR on fork");
+      close(connectionSocket);
     }
-    printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+    if (pid == 0) {
+      close(listenSocket);
 
-    // Send a Success message back to the client
-    charsRead = send(connectionSocket,
-                    "I am the server, and I got your message", 39, 0);
-    if (charsRead < 0){
-      error("ERROR writing to socket");
+      char input_line[32];
+      int length_tracker = 0;
+      char input_character;
+      while (length_tracker < (int)sizeof(input_line) - 1) {
+        recv(connectionSocket, &input_character, 1, 0);
+        if (input_character == '\n') {
+          break;
+        }
+        input_line[length_tracker++] = input_character;
+      }
+      input_line[length_tracker] = '\0';
+      int input_text_length = atoi(input_line);
+      char *input_text = malloc(input_text_length);
+      char *encoded_text = malloc(input_text_length);
+      char *key = malloc(input_text_length);
+
+      int data_received = 0;
+      while (data_received < input_text_length) {
+        ssize_t data = recv(connectionSocket,input_text + data_received,input_text_length - data_received,0);
+        data_received = data_received + data;
+      }
+      data_received = 0;
+      while (data_received < input_text_length) {
+        ssize_t data = recv(connectionSocket,key + data_received,input_text_length - data_received,0);
+        data_received = data_received + data;
+      }
+
+      for (int i = 0; i < input_text_length; i++) {
+        char input_char = input_text[i];
+        int input_char_value;
+        if (input_char == ' ') {
+          input_char_value = 26;
+        } else {
+          input_char_value = input_char - 'A';
+        }
+        char key_char = key[i];
+        int key_char_value;
+        if (key_char == ' ') {
+          key_char_value = 26;
+        } else {
+          key_char_value = key_char - 'A';
+        }
+        int encoded_value = (input_char_value + key_char_value) % 27;
+        if (encoded_value == 26) {
+          encoded_text[i] = ' ';
+        } else {
+          encoded_text[i] = 'A' + encoded_value;
+        }
+      }
+
+      size_t sent_counter = 0;
+      while (sent_counter < input_text_length) {
+        ssize_t data_out = send(connectionSocket,encoded_text + sent_counter,input_text_length - sent_counter,0);
+        sent_counter = sent_counter + data_out;
+      }
+      close(connectionSocket);
+      free(input_text);
+      free(encoded_text);
+      free(key);
+      exit(0);
     }
-    // Close the connection socket for this client
     close(connectionSocket);
+
   }
-  // Close the listening socket
-  close(listenSocket);
-  return 0;
 }
